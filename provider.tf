fc57bb61ef3d1782483f079660ade20b5bd062fa
maheshgetat <98541783+maheshgetat@users.noreply.github.com>
2023-03-22 14:18:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,14 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
+ backend "s3" {
+    bucket = "ccs-aws-eks-cluster-terraform-state"
+    key = "ccs-aws-eks-cluster/terraform.tfstate"
+    region = "us-east-1"
+   # dynamodb_table = "euw1-aws-eks-cluster-tf-state"
+    encrypt = true
+  }
+}
 
 
 
